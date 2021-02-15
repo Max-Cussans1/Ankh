a1b8d89b2421a53f51046f60ec2c77b2cde92033
Jackwildesign <57155140+Jackwildesign@users.noreply.github.com>
2021-02-15 07:36:16 +0000

    More Ui work to get the sub menus going.

---
+++ Ankh/Assets/Scripts/OfficerAssignement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficerAssignement : MonoBehaviour
+{
+
+    [SerializeField] Canvas officerAssignment;
+    // Start is called before the first frame update
+    void Start()
+    {
+        officerAssignment.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
