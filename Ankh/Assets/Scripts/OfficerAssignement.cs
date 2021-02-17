67d7d25e3a15c3c64e732879caba3f93a8195410
Jackwildesign <57155140+Jackwildesign@users.noreply.github.com>
2021-02-17 08:22:34 +0000

    Starting adding some functionality to hiring and firing officers
    This is partly done as I need to work out how to string text using TMP.

--- Ankh/Assets/Scripts/OfficerAssignement.cs
+++ Ankh/Assets/Scripts/OfficerAssignement.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OfficerAssignement : MonoBehaviour
 {
-
+    
     [SerializeField] Canvas officerAssignment;
+    [SerializeField] int officerTotal = 30;
+    [SerializeField] Text officerTotalText;
     // Start is called before the first frame update
     void Start()
     {
         officerAssignment.gameObject.SetActive(false);
+        officerTotalText.text = officerTotal.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("Total officers = " + officerTotal);
+    }
+
+    public void IncreaseOfficers()
+    {
+        officerTotal++;
+    }
+
+    public void DecreaseOfficers()
+    {
+        officerTotal--;
     }
 }
