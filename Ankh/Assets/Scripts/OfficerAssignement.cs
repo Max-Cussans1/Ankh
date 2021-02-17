ffcd883392583b6ac8cda5ab7084079133839bf7
Jackwildesign <57155140+Jackwildesign@users.noreply.github.com>
2021-02-17 19:50:52 +0000

    Got the buttons to work and the text to string for increasing and decreasing officers.

--- Ankh/Assets/Scripts/OfficerAssignement.cs
+++ Ankh/Assets/Scripts/OfficerAssignement.cs
@@ -9,11 +9,16 @@ public class OfficerAssignement : MonoBehaviour
     
     [SerializeField] Canvas officerAssignment;
     [SerializeField] int officerTotal = 30;
-    [SerializeField] Text officerTotalText;
+    [SerializeField] TMP_Text officerTotalText;
     // Start is called before the first frame update
     void Start()
     {
         officerAssignment.gameObject.SetActive(false);
+        UpdateOfficerTotal();
+    }
+
+    private void UpdateOfficerTotal()
+    {
         officerTotalText.text = officerTotal.ToString();
     }
 
@@ -26,10 +31,12 @@ public class OfficerAssignement : MonoBehaviour
     public void IncreaseOfficers()
     {
         officerTotal++;
+        UpdateOfficerTotal();
     }
 
     public void DecreaseOfficers()
     {
         officerTotal--;
+        UpdateOfficerTotal();
     }
 }
