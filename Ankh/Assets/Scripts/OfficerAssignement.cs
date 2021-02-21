932d2519050025c07a765eee2291a91b82b2f2d3
Jackwildesign <57155140+Jackwildesign@users.noreply.github.com>
2021-02-21 20:20:46 +0000

    Setting up instantiate object when you add officers
    I am just setting up the method to add objects, I just need to work out how to call the method from anther script, trying to find the best way to do this.

--- Ankh/Assets/Scripts/OfficerAssignement.cs
+++ Ankh/Assets/Scripts/OfficerAssignement.cs
@@ -10,35 +10,38 @@ public class OfficerAssignement : MonoBehaviour
     [SerializeField] Canvas officerAssignment;
     [SerializeField] int officerTotal = 0;
     [SerializeField] TMP_Text officerTotalText;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         officerAssignment.gameObject.SetActive(false);
         //Cannot work out how to find all of the officers in scene
-        //officerTotal = FindObjectsOfType(GameObject(Officer)).Legnth;
+        officerTotal = FindObjectsOfType(typeof(Officer)).Length;
         UpdateOfficerTotal();
     }
 
     private void UpdateOfficerTotal()
     {
+        officerTotal = FindObjectsOfType(typeof(Officer)).Length;
         officerTotalText.text = officerTotal.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Total officers = " + officerTotal);
+
     }
 
     public void IncreaseOfficers()
     {
-        officerTotal++;
+        //GameObject
         UpdateOfficerTotal();
     }
 
     public void DecreaseOfficers()
     {
-        officerTotal--;
+
         UpdateOfficerTotal();
     }
 }
