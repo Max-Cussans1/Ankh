22f1f1c734737c31c01a6fbf4b00b8abe08fc966
Jackwildesign <57155140+Jackwildesign@users.noreply.github.com>
2021-03-02 21:01:28 +0000

    Got a bit more stuff set up for the officer script
    Need to get more of it stringing when I add text

--- Ankh/Assets/Scripts/OfficerManagement.cs
+++ Ankh/Assets/Scripts/OfficerManagement.cs
@@ -1,26 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OfficerManagement : MonoBehaviour
 {
-    [SerializeField] Officer officerToInstantiate;
-    [SerializeField] Transform PoliceStation;
 
+    //Lists
     List<Officer> officerIdle = new List<Officer>();
     List<Officer> officerPatrol = new List<Officer>();
     List<Officer> officerInvestigation = new List<Officer>();
     List<Officer> officerRecords = new List<Officer>();
 
+    [SerializeField] Officer officerToInstantiate;
+    [SerializeField] Transform PoliceStation;
+    [SerializeField] int officerTotal = 0;
+
+    [Header("To String Objects")]
+    [SerializeField] TMP_Text patrollingOfficerTotal;
+    [SerializeField] TMP_Text investigatingOfficerTotal;
+    [SerializeField] TMP_Text recordsOfficerTotal;
+    [SerializeField] TMP_Text idleOfficerTotal;
+    [SerializeField] TMP_Text officerTotalText;
+
+    [Header("Canvases")]
+    [SerializeField] Canvas officerAssignment;
+
     void Start()
     {
-        
+        //Turns off the UI panel at run time (seems to be the best way to do it)
+        officerAssignment.gameObject.SetActive(false);
+        officerTotal = FindObjectsOfType(typeof(Officer)).Length;
+
+        UpdateOfficerTotal();
+        UpdatePatrollingTotal();
     }
+
+    private void UpdateOfficerTotal()
+    {
+        officerTotal = FindObjectsOfType(typeof(Officer)).Length;
+        officerTotalText.text = officerTotal.ToString();
+        print("Officer Total = " + officerTotal);
+    }
+
+    private void UpdatePatrollingTotal()
+    {
+        patrollingOfficerTotal.text = officerPatrol.Count.ToString() + "/" + officerTotal.ToString();
+    }
+
     public void HireNewOfficer()
     {
         Officer newOfficer = Instantiate(officerToInstantiate);
         officerIdle.Add(newOfficer);
-        print(officerIdle.Count + " Idle Officers");
+        UpdateOfficerTotal();
 
     }
 
@@ -29,17 +61,17 @@ public class OfficerManagement : MonoBehaviour
         
         if (officerIdle.Count <= 0)
         {
-            //Pop up a UI saying - no idle officers.
+            //Todo - Pop up a UI saying - no idle officers.
             print("No Officer to fire");
             return;
         }
         else
         {
             //Eventually this should remove the specific officer from the force for now we just remove one idle officer
-            //this just deletes the script
             Officer firedOfficer = officerIdle[officerIdle.Count-1];
             Destroy(firedOfficer.gameObject);
             officerIdle.Remove(officerIdle[officerIdle.Count -1]);
+            UpdateOfficerTotal();
         }
     }
     public void MoveIdleOfficerToPatrolling()
@@ -50,16 +82,13 @@ public class OfficerManagement : MonoBehaviour
             print("No Free Officers");
             return;
         }
-
     else
         {
             Officer idleToPatrol = officerIdle[officerIdle.Count - 1];
             officerIdle.Remove(idleToPatrol);
             officerPatrol.Add(idleToPatrol);
-            print(officerIdle.Count + " Idle officers");
-            print(officerPatrol.Count + " Patrolling officers");
+            UpdatePatrollingTotal();
         }
-
     }
 
     public void MoveIdleOfficerToInvestigation()
@@ -79,6 +108,5 @@ public class OfficerManagement : MonoBehaviour
             print(officerIdle.Count + " Idle officers");
             print(officerInvestigation.Count + " Investigating officers");
         }
-
     }
 }
